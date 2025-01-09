3c93e15630511cadeb7644566764da8580d0b905
5asul <drafasul@gmail.com>
2025-01-09 10:51:31 +0300

    Static Rendering

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,4 +1,16 @@
-import React from 'react'
+import { notFound } from "next/navigation"
+
+export const dynamicParams = true // default val = true
+
+export async function generateStaticParams() {
+  const res = await fetch('http://localhost:4000/tickets')
+
+  const tickets = await res.json()
+ 
+  return tickets.map((ticket) => ({
+    id: ticket.id
+  }))
+}
 
 async function getTicket(id) {
     const res = await fetch(`http://localhost:4000/tickets/${id}`, {
@@ -6,13 +18,17 @@ async function getTicket(id) {
         revalidate: 60
       }
     })
+
+    if (!res.ok) {
+        notFound()
+      }
   
     return res.json()
   }
 
 export default async function TicketDetails({ params }) {
-  // const id = params.id
-  const ticket = await getTicket(params.id)
+  const { id } = await params
+  const ticket = await getTicket(id)
 
   return (
     <main>
