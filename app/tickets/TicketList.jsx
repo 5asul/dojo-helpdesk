49777b54973a77f7348a8b70921e85a0fec2de83
5asul <drafasul@gmail.com>
2025-01-09 11:26:05 +0300

    Loading UI & Supsense

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 
 async function getTickets(){
+    // imitate delay
+    await new Promise(resolve => setTimeout(resolve, 3000))
     const res = await fetch('http://localhost:4000/tickets',{
         next: {
             revalidate: 0
