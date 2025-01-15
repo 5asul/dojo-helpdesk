e3f67c3da8c49b1f62a646c7561d9e883655dfde
5asul <drafasul@gmail.com>
2025-01-15 09:28:25 +0300

    Checking the Ticket Author

--- app/(dashboard)/tickets/[id]/page.jsx
+++ app/(dashboard)/tickets/[id]/page.jsx
@@ -2,6 +2,9 @@ import { notFound } from 'next/navigation'
 import { cookies } from 'next/headers'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
+//components
+import DeleteIcon from './DeleteIcon'
+
 export const dynamicParams = true // default val = true
 
 export async function generateMetadata({ params }) {
@@ -38,20 +41,27 @@ export default async function TicketDetails({ params }) {
   const { id } = await params
   const ticket = await getTicket(id)
 
+  const supabase = createServerComponentClient({ cookies })
+  const {data } = await supabase.auth.getSession()
+
   return (
     <main>
-      <nav>
-        <h2>Ticket Details</h2>
-      </nav>
-      <div className="card">
-        <h3>{ticket.title}</h3>
-        <small>Created by {ticket.user_email}</small>
-        <p>{ticket.body}</p>
-        <div className={`pill ${ticket.priority}`}>
-          {ticket.priority} priority
-        </div>
+    <nav>
+      <h2>Ticket Details</h2>
+      <div className="ml-auto">
+        {data.session.user.email === ticket.user_email && (
+          <DeleteIcon id={ticket.id} />
+        )}
       </div>
-      
-    </main>
+    </nav>
+    <div className="card">
+      <h3>{ticket.title}</h3>
+      <small>Created by {ticket.user_email}</small>
+      <p>{ticket.body}</p>
+      <div className={`pill ${ticket.priority}`}>
+        {ticket.priority} priority
+      </div>
+    </div>
+  </main>
   )
 }
