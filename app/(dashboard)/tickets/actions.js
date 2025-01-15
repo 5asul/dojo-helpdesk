f0ff545506dfa66ee9fef16b417caaa2a5a919b7
5asul <drafasul@gmail.com>
2025-01-15 12:10:07 +0300

    Handling Server Errors

--- app/(dashboard)/tickets/actions.js
+++ app/(dashboard)/tickets/actions.js
@@ -14,11 +14,15 @@ export async function addTicket(formData) {
     const { data: { session } } = await supabase.auth.getSession()
   
     // insert the data
-    const { error } = await supabase.from('Tickets')
+    const { error } = await supabase.from('Ticketes')
       .insert({
         ...ticket,
         user_email: session.user.email,
       })
+
+      if (error) {
+        throw new Error('Could not add the new ticket.')
+      }
   
     revalidatePath('/tickets')
     redirect('/tickets')
